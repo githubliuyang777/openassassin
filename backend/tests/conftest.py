bfdfa2eef2243b7e7018c2e70aeeccfdcdd15146
liuyanglinux <yang_liu0418@163.com>
2026-07-03 17:40:44 +0800

    chore: remove unused create_engine import

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,7 +1,6 @@
 import os
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 os.environ["DATABASE_URL"] = "sqlite:///./test.db"
